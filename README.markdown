69b9674ff0486bb2ecfa20e555b81670d57ab584
Gabriela Costa Sertori <64798344+gabrielasertori@users.noreply.github.com>
2021-04-03 22:50:26 -0300

    Super Trunfo

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Super Trunfo - Dia 8 GoT
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/gabrielasertori/pen/ExZZNaq](https://codepen.io/gabrielasertori/pen/ExZZNaq).
+
+
---
+++ src/script.js
@@ -0,0 +1,390 @@
+var carta1 = {
+  imagem: "https://data.whicdn.com/images/295382826/original.gif",
+  nome: "Daenerys",
+  atributos: {
+    ataque: 65,
+    defesa: 43,
+    magia: 89,
+    carisma: 37,
+    astucia: 35
+  }
+}
+
+var carta2 = {
+  imagem: "https://i.gifer.com/OJxT.gif",
+  nome: "Jon Snow",
+  atributos: {
+    ataque: 56,
+    defesa: 78,
+    magia: 62,
+    carisma: 70,
+    astucia: 68
+  }
+}
+
+var carta3 = {
+  imagem: "https://media.tenor.com/images/57316cf509e68a9f903e39c5b740a339/tenor.gif",
+  nome: "Cersei",
+  atributos: {
+    ataque: 32,
+    defesa: 40,
+    magia: 24,
+    carisma: 15,
+    astucia: 89
+  }
+}
+
+var carta4 = {
+  imagem: "https://media.tenor.com/images/d2ee336e905d12cb30fda06b17b4ddba/tenor.gif",
+  nome: "Bran",
+  atributos: {
+    ataque: 5,
+    defesa: 10,
+    magia: 95,
+    carisma: 32,
+    astucia: 67
+  }
+}
+
+var carta5 = {
+  imagem: "https://sm.ign.com/ign_br/screenshot/default/screenshot-4_15ym.png",
+  nome: "Montanha",
+  atributos: {
+    ataque: 97,
+    defesa: 94,
+    magia: 12,
+    carisma: 2,
+    astucia: 5
+  }
+}
+
+var carta6 = {
+  imagem: "https://thumbs.gfycat.com/CreamyFluidDuiker-small.gif",
+  nome: "Sandor 'Cão' Clegane",
+  atributos: {
+    ataque: 87,
+    defesa: 90,
+    magia: 17,
+    carisma: 10,
+    astucia: 10
+  }
+}
+
+var carta7 = {
+  imagem: "http://37.media.tumblr.com/5b6af01a846958600e3ed958e1b88324/tumblr_n9wbva3hmR1sfm4w6o1_250.gif",
+  nome: "Tyrion",
+  atributos: {
+    ataque: 10,
+    defesa: 20,
+    magia: 25,
+    carisma: 79,
+    astucia: 85
+  }
+}
+
+var carta8 = {
+  imagem: "https://media1.tenor.com/images/ff278432ebe7cd396be0e7293ad8cc1f/tenor.gif?itemid=14240132",
+  nome: "Jaime",
+  atributos: {
+    ataque: 80,
+    defesa: 76,
+    magia: 15,
+    carisma: 80,
+    astucia: 63
+  }
+}
+
+var carta9 = {
+  imagem: "https://capricho.abril.com.br/wp-content/uploads/2019/05/sansa-stark-game-of-thrones.png",
+  nome: "Sansa",
+  atributos: {
+    ataque: 42,
+    defesa: 50,
+    magia: 55,
+    carisma: 68,
+    astucia: 60
+  }
+}
+
+var carta10 = {
+  imagem: "https://media0.giphy.com/media/oxLsWbH1rvy2A/200.gif",
+  nome: "Ned",
+  atributos: {
+    ataque: 68,
+    defesa: 72,
+    magia: 50,
+    carisma: 76,
+    astucia: 58
+  }
+}
+
+var carta11 = {
+  imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI6Nc9csC-N8LJaSxigbCd3M8ATWa39gpDRheoCr14UPlC-P2OLb_FiQDVeGIzIZEd3mg&usqp=CAU",
+  nome: "Khal Drogo",
+  atributos: {
+    ataque: 86,
+    defesa: 72,
+    magia: 37,
+    carisma: 30,
+    astucia: 62
+  }
+}
+
+var carta12 = {
+  imagem: "https://media2.giphy.com/media/3pLxY4xH5ItIk/200w.gif",
+  nome: "Oberyn Martell",
+  atributos: {
+    ataque: 44,
+    defesa: 38,
+    magia: 22,
+    carisma: 66,
+    astucia: 58
+  }
+}
+
+var carta13 = {
+  imagem: "https://i.imgur.com/q2DRMow.gif",
+  nome: "Joffrey",
+  atributos: {
+    ataque: 70,
+    defesa: 27,
+    magia: 10,
+    carisma: 5,
+    astucia: 20
+  }
+}
+
+var carta14 = {
+  imagem: "https://64.media.tumblr.com/609cd136bc0cd6c0f836343a36ce8db1/tumblr_inline_pa0cho9pIz1swr0he_250.gifv",
+  nome: "Ramsay",
+  atributos: {
+    ataque: 60,
+    defesa: 54,
+    magia: 48,
+    carisma: 6,
+    astucia: 79
+  }
+}
+
+var carta15 = {
+  imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGBF8_AC_RCHk__NxEr4FQh9QP-HFVabvSQA&usqp=CAU",
+  nome: "Melisandre",
+  atributos: {
+    ataque: 42,
+    defesa: 56,
+    magia: 92,
+    carisma: 52,
+    astucia: 84
+  }
+}
+
+var carta16 = {
+  imagem: "https://i.pinimg.com/originals/f8/fa/51/f8fa51595375ad141f80fd6ceda8c05b.gif",
+  nome: "Rei da Noite",
+  atributos: {
+    ataque: 90,
+    defesa: 90,
+    magia: 95,
+    carisma: 7,
+    astucia: 80
+  }
+}
+
+var carta17 = {
+  imagem: "https://upload.wikimedia.org/wikipedia/pt/thumb/d/d5/Aidan_Gillen_playing_Petyr_Baelish.jpg/220px-Aidan_Gillen_playing_Petyr_Baelish.jpg",
+  nome: "Mindinho",
+  atributos: {
+    ataque: 42,
+    defesa: 54,
+    magia: 23,
+    carisma: 68,
+    astucia: 92
+  }
+}
+
+var carta18 = {
+  imagem: "https://pbs.twimg.com/profile_images/497472056229769216/5gR_7agA_400x400.png",
+  nome: "Hodor",
+  atributos: {
+    ataque: 5,
+    defesa: 25,
+    magia: 32,
+    carisma: 46,
+    astucia: 10
+  }
+}
+
+var carta19 = {
+  imagem: "http://pa1.narvii.com/6425/5ff7932dc625ba371a524e2bdebb1b1cf81357b8_00.gif",
+  nome: "Ygritte",
+  atributos: {
+    ataque: 76,
+    defesa: 74,
+    magia: 56,
+    carisma: 72,
+    astucia: 70
+  }
+}
+
+var carta20 = {
+  imagem: "https://i.pinimg.com/originals/86/b9/9d/86b99d5fdd6e1662810e749917595c1b.gif",
+  nome: "Brienne",
+  atributos: {
+    ataque: 90,
+    defesa: 90,
+    magia: 15,
+    carisma: 37,
+    astucia: 47
+  }
+}
+
+
+var cartas = [carta1, carta2, carta3, carta4, carta5, carta6, carta7, carta8, carta9, carta10, carta11, carta12, carta13, carta14, carta15, carta16, carta17, carta18, carta19, carta20]
+
+var cartaComputador 
+var cartaJogador
+var pontosJogador = 0
+var pontosComputador = 0
+var pontos = document.getElementById("contagem")
+pontos.innerHTML = "<h3>Jogador - " + pontosJogador + " | " + pontosComputador + " - Oponente</h3>"
+
+document.getElementById('btnSortear').disabled = false
+document.getElementById('btnJogar').disabled = true
+document.getElementById('btnNovaPartida').disabled = true
+
+
+atualizaDeck()
+
+function atualizaDeck() {
+  var divDeck = document.getElementById("quantidade-cartas")
+  divDeck.innerHTML = "<p>Cartas restantes: " + cartas.length + "</p>"
+}
+
+
+function sortearCarta() {
+    var numeroCartaComputador = parseInt(Math.random() * cartas.length)
+    cartaComputador = cartas[numeroCartaComputador]
+    cartas.splice(numeroCartaComputador, 1)
+
+    var numeroCartaJogador = parseInt(Math.random() * cartas.length)
+    cartaJogador = cartas[numeroCartaJogador]
+    cartas.splice(numeroCartaJogador, 1)
+    
+    
+    exibeCartaJogador()
+    atualizaDeck()
+  
+    var focaCarta = document.querySelector("#btnSortear")
+    focaCarta.scrollIntoView({behavior: "smooth"})
+
+    document.getElementById('btnSortear').disabled = true
+    document.getElementById('btnJogar').disabled = false
+    document.getElementById('btnNovaPartida').disabled = true
+}
+
+
+function exibeCartaJogador() {
+  var divCartaJogador = document.getElementById("carta-jogador")
+  var moldura = "<img src='https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png' style='width: inherit; height: inherit; position: absolute;'>";
+  divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`
+  var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`
+  
+  var opcoesTexto = ""
+  
+  for (var atributo in cartaJogador.atributos) {
+        opcoesTexto += "<input type='radio' name='atributo' style='margin:8px;' value='" + atributo + "'>" + atributo + " " + cartaJogador.atributos[atributo] + "<br>"
+    }
+  
+  var html = "<div id='opcoes' class='carta-status'>"
+  
+  divCartaJogador.innerHTML = nome + moldura + html + opcoesTexto + "</div>" 
+  
+}
+
+
+
+function obtemAtributoSelecionado() {
+    var radioAtributo = document.getElementsByName('atributo')
+    
+    for (var i = 0; i < radioAtributo.length; i++) {
+        if (radioAtributo[i].checked) {
+            return radioAtributo[i].value
+            
+        }
+         
+    }
+}
+
+
+
+function jogar() {
+    var divResultado = document.getElementById("resultado")
+    var atributoSelecionado = obtemAtributoSelecionado()
+    
+    if (atributoSelecionado != null) {
+        document.getElementById('btnJogar').disabled = true
+        document.getElementById("btnNovaPartida").disabled = false
+      
+        if (cartaJogador.atributos[atributoSelecionado] > cartaComputador.atributos[atributoSelecionado]) {
+            htmlResultado = "<p class='resultado-final'>Vitória!</p>"
+            pontosJogador++
+        } else if (cartaJogador.atributos[atributoSelecionado] < cartaComputador.atributos[atributoSelecionado]) {
+            htmlResultado = "<p class='resultado-final'>Você perdeu</p>"
+            pontosComputador++
+        } else {
+            htmlResultado = "<p class='resultado-final'>Empate</p>"
+        }
+      divResultado.innerHTML = htmlResultado
+      pontos.innerHTML = "<h3>Jogador - " + pontosJogador + " | " + pontosComputador + " - Oponente</h3>"
+      exibeCartaComputador()
+     }  
+  
+    var scrollNovaPartida = document.querySelector("#resultado")
+    scrollNovaPartida.scrollIntoView({behavior: "smooth"})
+  
+    if (cartas.length == 0) {
+      if (pontosJogador > pontosComputador) {
+        alert("Você venceu!")
+      } else if (pontosJogador < pontosComputador) {
+        alert("Você perdeu")
+      } else {
+        alert("Empate")
+      }
+    }
+  
+    document.getElementById('btnSortear').disabled = true
+    
+}
+
+
+
+function exibeCartaComputador() {
+  var divCartaComputador = document.getElementById("carta-maquina")
+  var moldura = "<img src='https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png' style='width: inherit; height: inherit; position: absolute;'>";
+  divCartaComputador.style.backgroundImage = `url(${cartaComputador.imagem})`
+  var nome = `<p class="carta-subtitle">${cartaComputador.nome}</p>`
+  
+  var opcoesTexto = ""
+  
+  for (var atributo in cartaComputador.atributos) {
+        opcoesTexto += "<p type='text' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaComputador.atributos[atributo]
+    }
+  
+  var html = "<div id='opcoes' class='carta-status'>"
+  
+  divCartaComputador.innerHTML = nome + moldura + html + opcoesTexto + "</div>" 
+}
+
+
+
+function novaPartida() {
+  var resetaImagem = document.getElementById("cartas")
+  resetaImagem.innerHTML = `<div id="carta-jogador" class="carta"></div> <div id="centrao"><button class="button-jogar" type="button" id="btnJogar" onclick="jogar()" disabled=false>Jogar</button><div id="resultado"></div></div> <div id="carta-maquina" class="carta"></div>`
+  
+  var topoPagina = document.querySelector("h1")
+  topoPagina.scrollIntoView({behavior: "smooth"})
+    
+  document.getElementById("btnSortear").disabled = false
+  document.getElementById("btnJogar").disabled = true
+  document.getElementById("btnNovaPartida").disabled = true
+}
